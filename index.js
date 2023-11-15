90a97104e0655f519a87b135ce6c8b413b7b6f5c
Mark Lai <laimark900317@gmail.com>
2023-11-16 01:54:58 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+app.use(express.json())
+
+// cors setting
+var cors = require('cors')
+app.use(cors({
+  origin: '*'
+}))
+
+const direction = require('./src/routes/direction.js')
+const autoComplete = require('./src/routes/autoComplete.js')
+
+app.use('/direction', direction)
+app.use('/autoComplete', autoComplete)
+
+
+app.listen(8080, function() {
+  console.log(' Http Express Server run in 8080');
+});
